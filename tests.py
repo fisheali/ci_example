5a104dc590f281158ffd31760d3a080e7290ab53
fisheali <fisheali@oregonstate.edu>
2022-07-23 19:23:30 -0400

    added 3rd test

--- tests.py
+++ tests.py
@@ -13,5 +13,10 @@ class TestCase(unittest.TestCase):
         self.assertNotEqual(task.my_func(), expected)
 
 
+    def test3(self):
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
+
+
 if __name__ == '__main__':
     unittest.main()
